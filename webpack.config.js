c0d4ce66feee0a76ddfb61578c7f21835d549479
cauli-flowers <ymatsuo@ciigale.com>
2017-03-26 10:35:39 +0900

    add web pack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,65 @@
-module.exports = {
-    entry: {js: './src/main.js'},
-    output: {path: __dirname + '/public', filename: 'bundle.js'},
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-        ],
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = [
+    {
+        entry: {
+            js: './src/main.js',
+        },
+        output: {
+            path: __dirname + '/public',
+            filename: 'bundle.js'
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                }
+            ]
+        },
+        devtool: 'source-map',
+        devServer: {
+            contentBase: './public',
+            port: 8080,
+            inline: true,
+            historyApiFallback: true
+        }
     },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: './public',
-        port: 8080,
-        inline: true,
-        historyApiFallback: true,
+    {
+        entry: {
+            css: './src/main.css',
+        },
+        output: {
+            path: __dirname + '/public',
+            filename: 'bundle.css'
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('css-loader!postcss-loader'),
+                },
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin('bundle.css'),
+            new webpack.LoaderOptionsPlugin({
+                  options: {
+                    context: __dirname + '/public',
+                    postcss: [
+                      require('postcss-easy-import')({glob: true})
+                    ]
+                  }
+            }),
+        ],
+        devtool: 'source-map',
+        devServer: {
+            contentBase: './public',
+            port: 8080,
+            inline: true,
+            historyApiFallback: true
+        }
     }
-};
+];
